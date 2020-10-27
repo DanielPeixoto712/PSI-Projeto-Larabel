3b834bfcd9c293187b84750a4fd9869d1330a3cc
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-27 17:24:09 +0000

    Update .editorconfig, .env.example, and 232 more files...

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device=width, initial-scale=1.0" name="viewport">
+    <title>Repro Power</title>
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link  rel="stylesheet" type="text/css" href="{{('jumbotrom.css')}}">
+    <title>Repro Power</title>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="">Inicio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="toggle navigation" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(actual)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Desativado</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="{{('http://example.com')}}" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledy="dropdown01">
+                        <a class="dropdown-item" href="criacaosites.html">Criação de Sites</a>
+                        <a class="dropdown-item" href="marktingdig.html">Markting Digital</a>
+                        <a class="dropdown-item" href="desing.html">Design</a>
+                    </div>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('login.form')}}">Login<span class="sr-only">(actual)</span></a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+    
+    <main role="main">
+        <div class="jumbotrom " >
+            <div class="container u2" >
+                <div class="container" align="center">
+            <h1 class="display-3 " ><br><br></h1>
+            <p style="color:white;" class="te"></p>
+            <p><a class="" href="" target="blank"></a></p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2>CRIAÇÃO DE SITES</h2>
+                    <p>Pretende criar um site com qualidade e creatividade. Criamos Sites que potenciam negócios. </p>
+                    <p><a class="btn btn-secondary " href="criacaosites.html" role="button">Ver detalhes &raquo;</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>MARKETING DIGITAL</h2>
+                    <p>Destaque o seu site com campanhas de markting digital no Google e no Facebook</p>
+                    <p><a class="btn btn-secondary " href="marktingdig.html" role="button">Ver detalhes &raquo;</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>DESIGN</h2>
+                    <p>Uma imagem criativa permite potenciar e diferenciar o seu negócio. Criamos logótipos que marcam.</p>
+                    <p><a class="btn btn-secondary " href="design.html" role="button">Ver detalhes &raquo;</a></p>
+                </div>
+
+
+<br><br><br><br><br><br><br><br><br><br>
+
+<div class="col-md-2 offset-md-1">
+                    <div  class="card" style="width: 18rem;" >
+  <img class="card-img-top" src="img/rp.jpg" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Reprogramções de centralinas!</h5>
+    <p class="card-text">Trabalhamos com prossiona</p>
+    <a href="https://www.sanzza.com/servicos/criacao-e-gestao-de-websites/criacao-de-websites/?gclid=CjwKCAjwn9v7BRBqEiwAbq1EyyAox1kdmlWERTFWC1Pv5ap32mhPOWpw2nR9r9AlvTxN0KyhccJ9MRoC_dsQAvD_BwE" target="blank" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-2 offset-md-1">
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/turbo1.png" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Preparação de turbos!</h5>
+    <p class="card-text">Melhoramos a performance do seu carro!</p>
+    <a href="https://www.google.pt/maps/place/Grandes+Planos+S.A./@41.3417066,-8.4791091,17z/data=!3m1!4b1!4m5!3m4!1s0xd24f58e776d1e03:0xda5ef49273bb8e1f!8m2!3d41.3417066!4d-8.4769204" target="blank" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-2 offset-md-1">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="img/cole.jpg" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Fabricamos a nossas próprias linhas de escape!</h5>
+    <p class="card-text">Conheça as nossas linhas!</p>
+    <a href="https://promo.amen.pt/promo-servidores-dedicados/?idl=5ece6cb98b086200506f45e5&idv=5a7d6f8c8b0862008223e5d8&idc=5ece698d8b08620050e9b253&PR=EG14&ida=5ece6f3d8b0862005b30e550" target="blank" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+
+
+            </div>
+            <hr>
+        </div>
+    </main>
+
+<br>
+
+<br><br>
+    <footer class="container">
+        <p>&copy;Repro Power 2020-2021</p>
+    </footer>
+
+    <script src="js/jquery.min.js" ></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+</html>
